7409ca887b32dcb172bfe65160cf5c54594fb0e9
Ad-rian27 <rian270627@gmail.com>
2026-07-13 10:36:12 +0530

    Added Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AddDonor from './components/AddDonor'
 import ViewDonors from './components/ViewDonors'
+import Home from './components/Home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
 
         <Routes>
 
-          <Route path='/' element={<AddDonor />} />
+          <Route path='/' element={<Home />} />
           <Route path='/add' element={<AddDonor />} />
           <Route path='/view' element={<ViewDonors />} />
 
